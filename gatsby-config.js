5fedc8fa8c7b8d1f4d93b17bcacfd744c9ce3588
vidhushi <vandanaranavidhu@gmail.com>
2020-05-03 02:47:10 +0530

    title commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Blogs`,
+    title: `Vandana Rana`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://delog-w3layouts.netlify.com/`,
     home: {
       title: `Hi! Everyone`,
-      description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
+      description: ` I am Vandana Rana, a Software Engineer.This blog is all about javaScript, React js, building javaScript application and all relatd to frontend development. Keep up with articles for more information :)`,
     },
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
     w3l_dom_key: `5e609f7a2d23fCF_Domain_verify` 
